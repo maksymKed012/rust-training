e9fe593945a2540b4468fde05fb52f60dd8e202c
Maksym Ked <maksym.ked@king.com>
2026-09-10 22:15:19 +0100

    m3 s3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use clap::Parser;
 use std::borrow::Cow;
+use std::fmt::Formatter;
 
 #[derive(Parser)]
 struct Args {
     #[arg()]
     pattern: String,
     #[arg()]
-    file_name:String,
+    file_name: String,
     #[arg(short = 'i', long)]
     ignore_case: bool,
     #[arg(short = 'n', long)]
@@ -20,7 +21,11 @@ fn compare_lines(line: &str, pattern: &str, ignore_case: bool, invert: bool) -> 
     if ignore_case {
         pattern = Cow::Owned(pattern.to_lowercase());
     }
-    if invert {!line.contains(pattern.as_ref())} else {line.contains(pattern.as_ref())}
+    if invert {
+        !line.contains(pattern.as_ref())
+    } else {
+        line.contains(pattern.as_ref())
+    }
 }
 
 fn build_output_string(line: &str, line_number: i32, show_line_number: bool) -> String {
@@ -31,16 +36,35 @@ fn build_output_string(line: &str, line_number: i32, show_line_number: bool) -> 
     format!("{} {}", line_str_, line)
 }
 
+#[derive(Debug)]
+enum GrepError {
+    FileNotFound(String),
+    IoError(std::io::Error),
+}
 
-fn main() {
+impl std::fmt::Display for GrepError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            GrepError::FileNotFound(error) => write!(f, "File not found error: {error}"),
+            GrepError::IoError(error) => write!(f, "IO error: {error}"),
+        }
+    }
+}
 
+fn run() -> Result<(), GrepError> {
     let cli_args = Args::parse();
     let pattern = &cli_args.pattern;
     let file_name = &cli_args.file_name;
     let ignore_case = cli_args.ignore_case;
     let invert = cli_args.invert;
 
-    let mut file_content = std::fs::read_to_string(file_name).expect("Failed to read the file");
+    let mut file_content = std::fs::read_to_string(file_name).map_err(|e| match e.kind() {
+        std::io::ErrorKind::NotFound => {
+            GrepError::FileNotFound(format!("File {file_name} not found. {}", e.to_string()))
+        }
+        _ => GrepError::IoError(e),
+    })?;
+    // expect("Failed to read the file");
     if ignore_case {
         file_content = file_content.to_lowercase();
     }
@@ -50,5 +74,13 @@ fn main() {
             let out_str = build_output_string(line, (i + 1) as i32, cli_args.line_number);
             println!("match found: {out_str}");
         }
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Error {e}");
+        std::process::exit(1);
     }
 }
